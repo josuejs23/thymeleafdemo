28a35bab7dc70012a66d2dc46a93d725e23b6201
Josue Zorrilla <josuezorrilla2302@gmail.com>
2023-08-28 18:10:53 -0400

    Added more validations

--- src/main/java/com/josuejs23/springboot/thymeleafdemo/Customer.java
+++ src/main/java/com/josuejs23/springboot/thymeleafdemo/Customer.java
@@ -1,7 +1,6 @@
 package com.josuejs23.springboot.thymeleafdemo;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +9,14 @@ public class Customer {
     @NotNull(message = "Is required!")
     @Size(min = 1, message = "Must be 1 or greater!")
     private String lastName;
+
+    @NotNull(message = "Value is required")
+    @Min(value=0, message="Must be equal or greater than zero")
+    @Max(value=10, message="Must be equal or less than then")
+    private Integer freeTickets;
+
+    @Pattern(regexp = "^[A-Za-z0-9]{5}", message = "Must have five chars/digits")
+    private String codePostal;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +32,21 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Integer getFreeTickets() {
+        return freeTickets;
+    }
+
+    public void setFreeTickets(Integer freeTickets) {
+        this.freeTickets = freeTickets;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
     }
 }
--- src/main/java/com/josuejs23/springboot/thymeleafdemo/CustomerController.java
+++ src/main/java/com/josuejs23/springboot/thymeleafdemo/CustomerController.java
@@ -1,15 +1,25 @@
 package com.josuejs23.springboot.thymeleafdemo;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // Add initBinder to convert trim input strings remove whitespaces
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model){
